367668781de42d02800b098c6e38e983845d8138
Vishal-vash <vishalvasishat@Vishal.local>
2019-12-27 23:36:26 +0530

    Updated comment only

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ db.then(result => {
       user.save();
     }
     app.listen(3000);
-    console.log("Server Connected and Running on port 3000 !!");
+    console.log("Server Connected and is Running on port 3000 !!");
   });
 });
